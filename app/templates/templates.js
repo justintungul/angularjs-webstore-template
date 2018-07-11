a3b5ec3687d518d732c2275aeb65794c050a4004
Justin Tungul <justin.tungul@gmail.com>
2018-07-10 18:28:06 -0700

    Update template-details.html for dynamic data from JSON

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -6,7 +6,7 @@ angular.module('templateStore.templates', ['ngRoute'])
       templateUrl: 'templates/templates.html',
       controller: 'TemplatesCtrl'
     }).
-    when('/templates/:templatesId', {
+    when('/templates/:templateId', {
       templateUrl: 'templates/template-details.html',
       controller: 'TemplateDetailsCtrl'
     });
@@ -18,6 +18,12 @@ angular.module('templateStore.templates', ['ngRoute'])
   });
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope', function($scope){
-  console.log('Deets');
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json').success(function(data){
+    $scope.template = $filter('filter')(data, function(d){
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+  });
 }]);
