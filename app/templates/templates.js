74042af015f8d0fdac8c88d8f86769c889b25881
Justin Tungul <justin.tungul@gmail.com>
2018-07-10 18:08:39 -0700

    Update template.html for dynamic data from JSON

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -14,7 +14,7 @@ angular.module('templateStore.templates', ['ngRoute'])
 
 .controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
   $http.get('json/templates.json').success(function(data){
-    console.log(data);
+    $scope.templates = data;
   });
 }])
 
